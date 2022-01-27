9717361f02992394fb987c18c32aa22e60961893
dhonstudio <doon13@gmail.com>
2022-01-27 14:15:51 +0700

    Set base_url; autoload form_validation; create register page

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,6 +7,8 @@ class Auth extends CI_Controller {
 
 	public function index()
 	{
+
+
         $data = [
             'lang'          => 'en',
             'title'         => 'SB Admin 2 - Login',
@@ -24,5 +26,36 @@ class Auth extends CI_Controller {
 		$this->load->view('auth');
 		$this->load->view('templates/copyright');
 		$this->load->view('templates/footer');
+	}
+
+    public function register()
+	{
+        $this->form_validation->set_rules('firstName', 'First Name', 'required|trim|alpha|min_length[2]|max_length[99]');
+        $this->form_validation->set_rules('lastName', 'Last Name', 'required|trim|alpha|min_length[2]|max_length[100]');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|max_length[255]');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[3]|max_length[20]');
+        $this->form_validation->set_rules('repeat_password', 'Repeat Password', 'required|trim|matches[password]');
+
+		if($this->form_validation->run() == false) {
+            $data = [
+                'lang'          => 'en',
+                'title'         => 'SB Admin 2 - Login',
+                'css'           => [
+                    $this->css['sb-admin'],
+                ],
+                'body_class'    => 'bg-primary',
+                'js'            => [
+                    $this->js['fontawesome5'],
+                    $this->js['bootstrap-bundle5'],
+                ],
+            ];
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('register');
+            $this->load->view('templates/copyright');
+            $this->load->view('templates/footer');
+        } else {
+            
+        }
 	}
 }
