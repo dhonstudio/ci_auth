428223cd28c616334d219c2d58fcc24006822243
dhonstudio <doon13@gmail.com>
2022-04-07 16:02:58 +0700

    Fixed environment for library

--- application/helpers/libraries_helper.php
+++ application/helpers/libraries_helper.php
@@ -1,7 +1,13 @@
 <?php
 
-$ci = get_instance();
+if (ENVIRONMENT == 'development') {
+    $path = '/../../';
+} else if (ENVIRONMENT == 'testing') {
+    $path = '../../../../../../';
+} else {
+    $path = '../../../../../';
+}
 
-require_once __DIR__ . '/../../assets/ci_helpers/style_helper.php';
-require_once __DIR__ . '/../../assets/ci_libraries/DhonAPI.php';
-require_once __DIR__ . '/../../assets/ci_libraries/DhonEmail.php';
+require_once __DIR__ . $path. 'assets/ci_helpers/style_helper.php';
+require_once __DIR__ . $path. 'assets/ci_libraries/DhonAPI.php';
+require_once __DIR__ . $path. 'assets/ci_libraries/DhonEmail.php';
