14f83419dbeabfa8ac1406985fc6231fec73e700
dhonstudio <doon13@gmail.com>
2022-06-05 23:54:59 +0700

    init prod

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'testing');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
@@ -94,7 +94,7 @@ switch (ENVIRONMENT) {
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-$system_path = '../../../system';
+$system_path = '../../system';
 
 /*
  *---------------------------------------------------------------
